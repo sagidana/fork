622abcbae7749da6b54345dfeaca3aee9fba2478
Sagi Dana <sagidana1@gmail.com>
2022-03-03 22:22:12 +0200

    adding scrolling + fixing movement

--- window.py
+++ window.py
@@ -55,13 +55,20 @@ class Window():
         cursor[0] = self.position[0] + self.window_cursor[0]
         cursor[1] = self.position[1] + self.window_cursor[1]
         
+        elog(f"window: ({self.window_cursor[0]}, {self.window_cursor[1]})")
+        elog(f"buffer: ({self.buffer_cursor[0]}, {self.buffer_cursor[1]})")
+
         self.stdscr.move(cursor[1], cursor[0])
 
     def draw(self):
+        self.stdscr.clear()
         index = 0
+        before = self.window_cursor[1]
+        first_line = self.buffer_cursor[1] - before
+
         for i in range(self.height):
             try:
-                line = self.buffer.lines[i]
+                line = self.buffer.lines[first_line + i]
                 self.stdscr.addstr( i, 
                                     self.position[1] + 0, 
                                     line[:self.width])
@@ -69,36 +76,61 @@ class Window():
 
         self.draw_cursor()
 
-    def scroll_up(self):
-        pass
+    def _scroll_up(self):
+        self.buffer_cursor[1] -= 1
 
-    def scroll_down(self):
-        pass
+        if self.remember > self.buffer_cursor[0]:
+            self.buffer_cursor[0] = self.remember
+            self.window_cursor[0] = self.remember
+            self.remember = 0
+
+        line_len = len(self.buffer.lines[self.buffer_cursor[1]]) - 1
+        if line_len < self.window_cursor[0]:
+            self.remember = self.window_cursor[0]
+            self.window_cursor[0] = line_len
+            self.buffer_cursor[0] = line_len
+
+    def _scroll_down(self):
+        self.buffer_cursor[1] += 1
+
+        if self.remember > self.buffer_cursor[0]:
+            self.buffer_cursor[0] = self.remember
+            self.window_cursor[0] = self.remember
+            self.remember = 0
+
+        line_len = len(self.buffer.lines[self.buffer_cursor[1]]) - 1
+        if line_len < self.window_cursor[0]:
+            self.remember = self.window_cursor[0]
+            self.window_cursor[0] = line_len
+            self.buffer_cursor[0] = line_len
 
     def resize(self, width, height):
         self.width = width
         self.height = height
 
+        elog(f"width:{self.width}, height:{self.height}")
+
+        if self.window_cursor[0] >= self.width - 1:
+            diff = self.window_cursor[0] - (self.width - 1)
+            for i in range(diff): self._move_left()
+        else: pass
+
+        if self.window_cursor[1] >= self.height - 1:
+            diff = self.window_cursor[1] - (self.height - 1)
+            for i in range(diff): self._move_up()
+        else: pass
+
         self.draw()
 
-    @raise_event
-    def move_up(self):
+    def _move_up(self):
+        # We are at the bottom
+        if self.buffer_cursor[1] == 0: return
+
+        # We need to scroll up
         if self.window_cursor[1] == 0:
-            if self.buffer_cursor[1] == 0: pass
-            else:
-                self.buffer_cursor[1] -= 1
-
-                if self.remember > self.buffer_cursor[0]:
-                    self.buffer_cursor[0] = self.remember
-                    self.window_cursor[0] = self.remember
-                    self.remember = 0
-
-                line_len = len(self.buffer.lines[self.buffer_cursor[1]]) - 1
-                if line_len < self.window_cursor[0]:
-                    self.remember = self.window_cursor[0]
-                    self.window_cursor[0] = line_len
-                    self.buffer_cursor[0] = line_len
-        else:
+            self._scroll_up()
+            self.draw()
+        else: # Simple up action
             if self.buffer_cursor[1] == 0: raise Exception('Should never happen.')
 
             self.window_cursor[1] -= 1
@@ -114,26 +146,16 @@ class Window():
                 self.remember = self.window_cursor[0]
                 self.window_cursor[0] = line_len
                 self.buffer_cursor[0] = line_len
-        self.draw_cursor()
 
-    @raise_event
-    def move_down(self):
+    def _move_down(self):
+        # We are at the bottom
         if self.buffer_cursor[1] == len(self.buffer.lines) - 1: return
 
+        # We need to scroll down
         if self.window_cursor[1] == self.height - 1:
-            self.buffer_cursor[1] += 1
-
-            if self.remember > self.buffer_cursor[0]:
-                self.buffer_cursor[0] = self.remember
-                self.window_cursor[0] = self.remember
-                self.remember = 0
-
-            line_len = len(self.buffer.lines[self.buffer_cursor[1]]) - 1
-            if line_len < self.window_cursor[0]:
-                self.remember = self.window_cursor[0]
-                self.window_cursor[0] = line_len
-                self.buffer_cursor[0] = line_len
-        else:
+            self._scroll_down()
+            self.draw()
+        else: # Simple down action
             self.window_cursor[1] += 1
             self.buffer_cursor[1] += 1
 
@@ -147,11 +169,8 @@ class Window():
                 self.remember = self.window_cursor[0]
                 self.window_cursor[0] = line_len
                 self.buffer_cursor[0] = line_len
-        self.draw_cursor()
 
-    @raise_event
-    def move_right(self):
-        elog(f"{len(self.buffer.lines[self.buffer_cursor[1]])}")
+    def _move_right(self):
         if self.buffer_cursor[0] == len(self.buffer.lines[self.buffer_cursor[1]]) - 1:
             return
 
@@ -161,10 +180,8 @@ class Window():
             pass # TODO scroll horizontally
         else:
             self.window_cursor[0] += 1
-        self.draw_cursor()
 
-    @raise_event
-    def move_left(self):
+    def _move_left(self):
         if self.buffer_cursor[0] == 0: return
 
         self.buffer_cursor[0] -= 1
@@ -173,4 +190,24 @@ class Window():
             pass # TODO scroll horizontally
         else:
             self.window_cursor[0] -= 1
+            self.remember = 0
+
+    @raise_event
+    def move_up(self):
+        self._move_up()
+        self.draw_cursor()
+
+    @raise_event
+    def move_down(self):
+        self._move_down()
+        self.draw_cursor()
+
+    @raise_event
+    def move_right(self):
+        self._move_right()
+        self.draw_cursor()
+
+    @raise_event
+    def move_left(self):
+        self._move_left()
         self.draw_cursor()
