073f611647465dee62a4608eaa02767bf813c07b
Sagi Dana <sagidana1@gmail.com>
2022-03-09 23:52:20 +0200

    working on python grammar and scope mappings

--- theme_demo.py
+++ theme_demo.py
@@ -2,6 +2,7 @@
 
 from tree_sitter import Language, Parser
 import json
+import re
 
 # Language.build_library(
         # # Store the library in the `build` directory
@@ -16,27 +17,111 @@ import json
 PY_LANGUAGE = Language('build/my-languages.so', 'python')
 
 
-def walk_recursive(node, cb, level=0):
-    cb(node, level)
+def walk(node, cb, level=0, nth_child=0):
+    cb(node, level, nth_child)
+    curr_nth_child = 0
     for child in node.children:
-        walk_recursive(child, cb, level + 1)
+        walk(child, cb, level + 1, curr_nth_child)
+        curr_nth_child += 1
+    
+def traverse_tree(tree):
+    cursor = tree.walk()
+    reached_root = False
+
+    while reached_root == False:
+        yield cursor
+
+        if cursor.goto_first_child(): continue
+
+        if cursor.goto_next_sibling(): continue
+
+        retracing = True
+
+        while retracing:
+            if not cursor.goto_parent(): 
+                retracing = False
+                reached_root = True
+
+            if cursor.goto_next_sibling():
+                retracing = False
+
+def make_move(cursor, move, fn):
+    if (move == "down"):
+        fn(cursor)
+        if (cursor.goto_first_child()):
+            make_move(cursor, "down", fn)
+        elif (cursor.goto_next_sibling()):
+            make_move(cursor, "right", fn)
+        elif (cursor.goto_parent()):
+            make_move(cursor, "up", fn)
+    elif (move == "right"):
+        fn(cursor)
+        if (cursor.goto_first_child()):
+            make_move(cursor, "down", fn)
+        elif (cursor.goto_next_sibling()):
+            make_move(cursor, "right", fn)
+        elif (cursor.goto_parent()):
+            make_move(cursor, "up", fn)
+    elif move == "up":
+        if (cursor.goto_next_sibling()):
+            make_move(cursor, "right", fn)
+        elif (cursor.goto_parent()):
+            make_move(cursor, "up", fn)
 
 def get_scope_style(token_colors, scope):
     for token in token_colors:
         if "settings" not in token: continue
         if "scope" not in token: continue
-        if token['scope'] != scope: continue
 
-        # print(f"{token['settings']}")
-        return token['settings']
+        if isinstance(token['scope'], list):
+            for curr in token['scope']:
+                if curr == scope:
+                    # print(f"{scope}: {token['settings']}")
+                    return token['settings']
+            continue
+        else:
+            scopes = [x.strip() for x in token['scope'].split(',')]
 
-def map_node_to_scope(node, grammar):
-    # print(f"{node.type}")
-    scopes = grammar['scopes']
-    if node.type in scopes: return scopes[node.type]
-    if f"\"{node.type}\"" in scopes: return scopes[f"\"{node.type}\""]
+            for s in scopes:
+                if s == scope:
+                    return token['settings']
+            # if scope in scopes: return token['settings']
 
-    return None
+            # continue
+
+            # if scope != token['scope']: continue
+            # return token['settings']
+# 
+def map_node_to_scope(node, grammar, nth_child=0):
+    if 'match' in grammar:
+        pattern = grammar['match']
+
+        if re.match(pattern, node.text.decode('utf-8')): 
+            return grammar['scope']
+        return None
+    
+    _type = node.type
+    if _type not in grammar: 
+        _type = f"nth-child({nth_child})"
+        if _type not in grammar: return None
+
+    if isinstance(grammar[_type], str): return grammar[_type]
+
+    if isinstance(grammar[_type], list): 
+        for curr in grammar[_type]:
+            if not isinstance(curr, dict): return curr
+
+            parent = node.parent
+            if 'match' in curr: parent = node
+
+            return map_node_to_scope(parent, curr)
+
+    if isinstance(grammar[_type], dict):
+        parent = node.parent
+        if 'match' in grammar[_type]: parent = node
+
+        # print(f"{parent.type} > {node.type}")
+        return map_node_to_scope(parent, grammar[_type])
 
 def get_grammar(file_path):
     with open(file_path) as f:
@@ -57,17 +142,28 @@ def highlight_file(file_path):
     # print(json.dumps(grammar, indent=4))
 
     tree = parser.parse(file_bytes)
+    # cursor = tree.walk()
+    # print(dir(tree.root_node))
+    # return
 
-    def callback(node, level):
-        # print("  "*level + f"{node.type}")
-        scope = map_node_to_scope(node, grammar)
+    def callback(node, level, nth_child):
+        # node = cursor.node
+        scope = map_node_to_scope(node, grammar, nth_child)
         if not scope: return
 
+
         style = get_scope_style(token_colors, scope)
-        if not style: return 
+        if not style: 
+            print(f"{scope}")
+            return 
 
-        print(f"{scope}: {style}")
+        # print(f"{scope}: {style}")
 
-    walk_recursive(tree.root_node, callback)
+    walk(tree.root_node, callback)
+    # make_move(cursor, "down", callback)
+    # for cursor in traverse_tree(tree):
+        # print(cursor.node.child_count)
+        # break
+        # callback(cursor)
 
 highlight_file("editor")
