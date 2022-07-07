afdd6e3f5d3e6f694ade5e0f2bf7d5a7d0874597
Your Name <you@example.com>
2022-07-07 16:45:51 +0300

    fix the search bug

--- buffer.py
+++ buffer.py
@@ -182,11 +182,9 @@ class Buffer():
 
     def set_highlights(self, highlights):
         self.highlights = highlights
-        self._raise_event(ON_BUFFER_CHANGE, change)
 
     def clear_highlights(self):
         self.highlights = []
-        self._raise_event(ON_BUFFER_CHANGE, change)
 
     def add_highlights(self, highlights): pass
 
