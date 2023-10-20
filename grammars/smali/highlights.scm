9a905b56583716ffb291664897d6f35b8781f44f
Sagi Dana <sagidana1@gmail.com>
2023-10-20 15:19:51 +0000

    adding some highlights for smali

--- grammars/smali/highlights.scm
+++ grammars/smali/highlights.scm
@@ -7,13 +7,27 @@
   ".super"
   ".source"
   ".method"
+  ".end method"
+  ".locals"
+  ".param"
+  ".end param"
+  ".annotation"
+  ".end annotation"
+  ".field"
+  ".catch"
+  ".implements"
+  ".catchall"
 ] @keyword
+
 (line_directive) @keyword
 
 (opcode) @keyword
 (number) @constant.numeric
 (comment) @comment
 (string) @string
-(method_identifier) @entity.name.function
 (class_identifier) @support.type
-(variable) @variable
+(method_identifier) @entity.name.function
+(field_identifier) @entity.name.function
+(access_modifier) @entity.other.attribute-name
+(label) @entity.name.tag
+(parameter) @variable.parameter
