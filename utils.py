32a5bbf7a26976c51bd55ed6113c3b1faf358931
Sagi Dana <sagidana1@gmail.com>
2024-06-15 19:23:14 +0300

    fix goto file

--- utils.py
+++ utils.py
@@ -11,21 +11,24 @@ def is_binary_file(file):
 
 def extract_destination(string):
     file_path = None
-    file_line = None
-    file_col = None
+    file_line = 0
+    file_col = 0
 
     try:
         m = re.match(f'^(?P<file_path>[\\w\\.\\/\\-]+)(:(?P<file_line>\\d+):(?P<file_col>\\d+))?.*$',
                     string)
-        if not m: return None, None, None
+        if not m: return None, 0, 0
 
         file_path = m.group('file_path')
         file_line = m.group('file_line')
         if file_line: file_line = int(file_line) - 1
         file_col = m.group('file_col')
         if file_col: file_col = int(file_col) - 1
-        if not path.isfile(file_path): return None, None, None
-    except Exception as e: elog(f"Exception: {e}")
+        if not path.isfile(file_path): return None, 0, 0
+    except Exception as e: elog(f"[!] extract_destination: {e}")
+
+    if file_line is None: file_line = 0
+    if file_col is None: file_col = 0
 
     return file_path, file_line, file_col
 
