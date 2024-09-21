08e2f91b71706bef2528521902dc6b2b06d04836
Sagi Dana <sagidana1@gmail.com>
2024-09-21 14:00:31 +0000

    making setup.py a bit more concise

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = fh.read()
-
 setup(
     name = 'fork',
     version = '0.0.1',
@@ -12,16 +7,12 @@ setup(
     author_email = 'sagidana1@gmail.com',
     license = 'MIT License',
     description = 'fork editor',
-    long_description = long_description,
+    long_description = open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type = "text/markdown",
     url = 'https://github.com/sagidana/Editor',
     packages = find_packages(),
     include_package_data=True,
-    install_requires = [requirements],
+    install_requires = [open("requirements.txt", "r", encoding="utf-8").read()],
     python_requires='>=3.11',
-    classifiers=[
-        "Programming Language :: Python :: 3.8",
-        "Operating System :: OS Independent",
-    ],
     entry_points = {'console_scripts': "fork=fork:main"}
 )
